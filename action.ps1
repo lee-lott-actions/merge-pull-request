7f39006248edc8c648e2a0d1da118c266545fac4
Lee Lott <llott88@gmail.com>
2026-01-20 05:46:35 -0600

    feat: Add code to repo

--- action.ps1
+++ action.ps1
@@ -1,28 +1,60 @@
-#!/usr/bin/env pwsh
+function Merge-Pull-Request {
+  param(
+    [string]$RepoName,
+    [string]$OrgName,
+    [string]$PrNumber,
+    [ValidateSet("merge", "squash", "rebase")]
+    [string]$MergeType,
+    [string]$MergeTitleMessage,
+    [string]$Token
+  )
+  
+  # Validate required inputs
+  if ([string]::IsNullOrEmpty($RepoName) -or 
+      [string]::IsNullOrEmpty($OrgName) -or 
+      [string]::IsNullOrEmpty($PrNumber) -or 
+      [string]::IsNullOrEmpty($MergeType) -or 
+      [string]::IsNullOrEmpty($MergeTitleMessage) -or
+      [string]::IsNullOrEmpty($Token)) 
+  {
+    Write-Output "Error: Missing required parameters"
+    Add-Content -Path $env:GITHUB_OUTPUT -Value "error-message=Missing required parameters: RepoName, OrgName, PrNumber, MergeType, MergeTitleMessage, and Token must be provided."
+    Add-Content -Path $env:GITHUB_OUTPUT -Value "result=failure"
+    return
+  }
+  
+  $githubApiUrl = $env:MOCK_API
+  if (-not $githubApiUrl) { $githubApiUrl = "https://api.github.com" }
+  
+  $uri = "$githubApiUrl/repos/$OrgName/$RepoName/pulls/$PrNumber/merge"
 
-function open_issue {
-    param (
-        [string]$Input1,
-        [string]$Input2,
-        [string]$Input3
-    )
-
-    # Validate required inputs
-    if (
-        [string]::IsNullOrEmpty($Input1) -or
-        [string]::IsNullOrEmpty($Input2) -or
-        [string]::IsNullOrEmpty($Input3)
-    ) {
-        Write-Output "Error: Missing required parameters"
-        Add-Content -Path $env:GITHUB_OUTPUT -Value "error-message=Missing required parameters: Input1, Input2, and Input3 must be provided."
-        Add-Content -Path $env:GITHUB_OUTPUT -Value "result=failure"
-        return
-    }
-
-    Write-Output "Attempting to run action"
-
-    # Use MOCK_API if set, otherwise default to GitHub API
-    $ApiBaseUrl = if ($env:MOCK_API) { $env:MOCK_API } else { "https://api.github.com" }
-
-    //Your code goes here
+  $headers = @{
+      Authorization = "Bearer $Token"
+      Accept = "application/vnd.github+json"
+      "X-GitHub-Api-Version" = "2022-11-28"
+      "User-Agent" = "pwsh-action"
+  }
+  
+  $body = @{
+      commit_title = $MergeTitleMessage
+      merge_method = "$MergeType"
+  } | ConvertTo-Json
+  
+  try {
+      Write-Host "Merging Pull Request..."
+      $response = Invoke-WebRequest -Uri $uri -Headers $headers -Method PUT -Body $body
+ 
+     if ($response.StatusCode -eq 200) {
+          "result=success" | Out-File -FilePath $env:GITHUB_OUTPUT -Append
+          Write-Host "Pull Request #$PrNumber in repository $RepoName merged with merge type $MergeType. Status: $($response.StatusCode)"
+      } else {
+          "result=failure" | Out-File -FilePath $env:GITHUB_OUTPUT -Append
+          "error-message=Merge failed with status code $($response.StatusCode)." | Out-File -FilePath $env:GITHUB_OUTPUT -Append
+          Write-Host "Merged failed with status code $($response.StatusCode)."
+      }      
+  } catch {
+    "result=failure" | Out-File -FilePath $env:GITHUB_OUTPUT -Append
+    "error-message=Merge threw an exception and failed." | Out-File -FilePath $env:GITHUB_OUTPUT -Append
+    Write-Error "Failed to merge pull request: $_"      
+  }
 }
